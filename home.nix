ab6c1821af66a8947107cbf35aa0e23b72cea1ce
Alexander Weidemann <walnuss0815@gmail.com>
2024-03-20 22:19:24 +0100

    Revert "Remove unused tools"
    This reverts commit 2733ce722c8bfab03d4ecc3b7473f3a50e7e813a.

--- home.nix
+++ home.nix
@@ -126,6 +126,33 @@
     };
   };
 
+  # starship - an customizable prompt for any shell
+  programs.starship = {
+    enable = true;
+    # custom settings
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
+  # alacritty - a cross-platform, GPU-accelerated terminal emulator
+  programs.alacritty = {
+    enable = true;
+    # custom settings
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 12;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
