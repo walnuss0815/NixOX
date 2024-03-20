daa44684413611081d386f1e030b4a91c7648aab
Alexander Weidemann <walnuss0815@gmail.com>
2024-03-20 22:06:57 +0100

    Git config

--- home.nix
+++ home.nix
@@ -100,6 +100,44 @@
     enable = true;
     userName = "Alexander Weidemann";
     userEmail = "walnuss0815@gmail.com";
+    aliases = {
+      co = "checkout";
+      br = "branch";
+      ci = "commit";
+      st = "status";
+      unstage = "reset HEAD --";
+      up = "fetch --prune --all";
+      graph = "log --oneline --graph --decorate";
+      fpush = "push --force-with-lease";
+      mr = "push --push-option=merge_request.create --push-option=merge_request.draft";
+      wip = "commit --all --message='WIP'";
+      track = "add --intent-to-add";
+      tow = "pull --recurse-submodules=on-demand";
+      irb = "rebase --interactive --autosquash";
+      repo = "remote --verbose";
+      fuck = "commit --fixup";
+      pfuck = "commit --patch --fixup";
+      fix = "commit --amend --no-edit";
+      pfix = "commit --amend --no-edit --patch";
+      hash = "rev-parse HEAD";
+      sub = "submodule";
+      edit = "commit --amend --only";
+    };
+    extraConfig = {
+      merge = {
+        ff = "only";
+      };
+      pull = {
+        ff = "only";
+      };
+      push = {
+        autoSetupRemote = true;
+      };
+      init = {
+        defaultBranch = "main";
+      };
+    };
+
   };
 
   # starship - an customizable prompt for any shell
