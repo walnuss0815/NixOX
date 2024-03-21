98a7f764010ac4b85e9db11beff5691a729ee912
Alexander Weidemann <walnuss0815@gmail.com>
2024-03-21 10:53:12 +0100

    Minimize and maximize buttons

--- home.nix
+++ home.nix
@@ -23,6 +23,7 @@
   dconf = {
     enable = true;
     settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    settings."org/gnome/desktop/wm/preferences".button-layout = "minimize,maximize,close";
   };
 
   # set cursor size and dpi for 4k monitor
