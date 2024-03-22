312030ce567c21d77b215f0de7c9b9df9b47bec4
Alexander Weidemann <walnuss0815@gmail.com>
2024-03-22 18:33:17 +0100

    remove flatpak support

--- hosts/alexander-nb/configuration.nix
+++ hosts/alexander-nb/configuration.nix
@@ -73,9 +73,6 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  # Enable Flatpak
-  services.flatpak.enable = true;
-
   # Configure keymap in X11
   services.xserver = {
     layout = "de";
