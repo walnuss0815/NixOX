3d2073897768700febe7ef737ae70e60f2f0df13
Alexander Weidemann <walnuss0815@gmail.com>
2024-07-03 15:44:14 +0200

    remove console config

--- hosts/frigate/configuration.nix
+++ hosts/frigate/configuration.nix
@@ -23,11 +23,6 @@
   time.timeZone = "Europe/Berlin";
 
   i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-    useXkbConfig = true; # use xkb.options in tty.
-  };
 
   users.users.alexander = {
     isNormalUser = true;
