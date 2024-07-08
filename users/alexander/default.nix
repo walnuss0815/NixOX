74bd4647884334c1c2a8c77e296bbdc1146e7165
Alexander Weidemann <walnuss0815@gmail.com>
2024-07-08 12:00:31 +0200

    feat(user): add face

--- users/alexander/default.nix
+++ users/alexander/default.nix
@@ -4,6 +4,10 @@
   home.username = "alexander";
   home.homeDirectory = "/home/alexander";
 
+  home.file.".face" = {
+    source = ./img/face.jpg;
+  };
+
   # link the configuration file in current directory to the specified location in home directory
   # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
 
