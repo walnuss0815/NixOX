d41023710d29770be7bddcc5f75d5e556c1e729d
Alexander Weidemann <walnuss0815@gmail.com>
2024-07-17 14:07:19 +0200

    fix(alexander): fix default browser

--- users/alexander/default.nix
+++ users/alexander/default.nix
@@ -28,6 +28,10 @@
     enable = true;
     defaultApplications = {
       "text/html" = "google-chrome.desktop";
+      "x-scheme-handler/http" = "google-chrome.desktop";
+      "x-scheme-handler/https" = "google-chrome.desktop";
+      "x-scheme-handler/about" = "google-chrome.desktop";
+      "x-scheme-handler/unknown" = "google-chrome.desktop";
       "application/pdf" = "org.gnome.Evince.desktop";
     };
   };
