69bb302edfaa34c18cb5eeed9bd9da2bee1810e3
Alexander Weidemann <walnuss0815@gmail.com>
2024-09-04 10:41:17 +0200

    fix(netbird): disable autostart

--- modules/system/netbird/default.nix
+++ modules/system/netbird/default.nix
@@ -1,4 +1,5 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
   services.netbird.enable = true; # for netbird service & CLI
   environment.systemPackages = [ pkgs.netbird-ui ]; # for GUI
+  systemd.services.netbird-wt0.wantedBy = lib.mkForce []; # disable autostart
 }
